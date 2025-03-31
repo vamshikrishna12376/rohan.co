053071513967f6bc6d1f6d6fa4cd08da1228b485
RohanK-coder <kyrohan7730@gmail.com>
2025-04-01 00:07:59 +0530

    added-shop-list

---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,8 @@
+
+export default function Banner() {
+  return (
+    <div className="w-full h-[10vh] bg-black flex items-center justify-center">
+      <h1 className="text-white italic text-3xl">"where style meets compatability"</h1>
+    </div>
+  )
+}
